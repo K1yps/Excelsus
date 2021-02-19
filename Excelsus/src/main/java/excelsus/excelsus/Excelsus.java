4197418dd2068cf581e1b7d28f022a8cbe46a86d
K1yps <henriquegsneto@gmail.com>
2021-02-19 11:38:11 +0000

    Initial commit

---
+++ Excelsus/src/main/java/excelsus/excelsus/Excelsus.java
@@ -0,0 +1,10 @@
+package excelsus.excelsus;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Excelsus implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ Excelsus/src/main/java/excelsus/excelsus/client/ExcelsusClient.java
@@ -0,0 +1,13 @@
+package excelsus.excelsus.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class ExcelsusClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
