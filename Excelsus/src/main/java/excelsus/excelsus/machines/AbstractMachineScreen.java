e3ed483edd30daad92cf1368d8170e28350e3fd8
K1yps <henriquegsneto@gmail.com>
2021-02-24 20:49:42 +0000

    43242

--- Excelsus/src/main/java/excelsus/excelsus/machines/AbstractMachineScreen.java
+++ Excelsus/src/main/java/excelsus/excelsus/machines/AbstractMachineScreen.java
@@ -1,2 +1,18 @@
-package excelsus.excelsus.machines;public class AbstractMachineScreen {
+package excelsus.excelsus.machines;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.text.Text;
+
+@Environment(EnvType.CLIENT)
+public abstract class AbstractMachineScreen<T extends ScreenHandler> extends HandledScreen<T> {
+
+    public AbstractMachineScreen(T handler, PlayerInventory inventory, Text title) {
+        super(handler, inventory, title);
+    }
+
+
 }
--- Excelsus/src/main/java/excelsus/excelsus/machines/AbstractMachineScreenHandler.java
+++ Excelsus/src/main/java/excelsus/excelsus/machines/AbstractMachineScreenHandler.java
@@ -1,4 +1,13 @@
 package excelsus.excelsus.machines;
 
-public class AbstractMachineScreen {
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.screen.ScreenHandlerType;
+import org.jetbrains.annotations.Nullable;
+
+public abstract class AbstractMachineScreenHandler extends ScreenHandler {
+
+    protected AbstractMachineScreenHandler(@Nullable ScreenHandlerType<?> type, int syncId) {
+        super(type, syncId);
     }
+
+}
